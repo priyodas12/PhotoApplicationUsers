edeccb2bcb436bf63447159c3d6c477b8cf673c5
Priyobrato Das <priyodas12@gmail.com>
2020-01-24 10:06:09 +0530

    Add files via upload

---
+++ UserDto.java
@@ -0,0 +1,48 @@
+package com.workspace.shared;
+
+import java.io.Serializable;
+
+
+public class UserDto implements Serializable{
+	
+	private static final long serialVersionUID = -2304123665744749288L;
+	
+	private String username;
+	private String password;
+	private String email;
+	private String userid;
+	private String encryptedpassword;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getEncryptedpassword() {
+		return encryptedpassword;
+	}
+	public void setEncryptedpassword(String encryptedpassword) {
+		this.encryptedpassword = encryptedpassword;
+	}
+	
+}
